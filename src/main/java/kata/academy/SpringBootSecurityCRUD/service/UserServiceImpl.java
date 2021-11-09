eb1d0f88f321426abe92de69c3864f5ae1faa692
Роман Мазниченко <87892908+feromant@users.noreply.github.com>
2021-11-09 10:56:38 +0300

    Add files via upload

---
+++ src/main/java/kata/academy/SpringBootSecurityCRUD/service/UserServiceImpl.java
@@ -0,0 +1,86 @@
+package kata.academy.SpringBootSecurityCRUD.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import kata.academy.SpringBootSecurityCRUD.dao.UserDao;
+import kata.academy.SpringBootSecurityCRUD.model.Role;
+import kata.academy.SpringBootSecurityCRUD.model.User;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService {
+    private final UserDao dao;
+
+    public UserServiceImpl(UserDao dao) {
+        this.dao = dao;
+    }
+
+    @Autowired
+    private BCryptPasswordEncoder passwordEncoder;
+
+    @Transactional
+    @Override
+    public void addUser(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        dao.saveUser(user);
+    }
+
+    @Transactional(readOnly = true)
+    @Override
+    public List<User> getAllUsers() {
+        return dao.getAllUsers();
+    }
+
+    @Transactional(readOnly = true)
+    @Override
+    public User getUserById(Long id) {
+        return dao.getUserById(id);
+    }
+
+    @Transactional(readOnly = true)
+    @Override
+    public User getUserByUsername(String username) {
+        return dao.getUserByName(username);
+    }
+
+    @Transactional
+    @Override
+    public void updateUser(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        dao.updateUser(user);
+    }
+
+    @Transactional
+    @Override
+    public void deleteUserById(Long id) {
+        dao.deleteUserById(id);
+    }
+
+    @Transactional(readOnly = true)
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = dao.getUserByName(username);
+        if(user == null) {
+            throw new UsernameNotFoundException(String.format("User '%s' not found", username));
+        }
+        return new org.springframework.security.core.userdetails.User(user.getUsername(), user.getPassword(),
+                true, true, true, true, getGrantedAuthorities(user));
+    }
+
+    private List<GrantedAuthority> getGrantedAuthorities(User user){
+        List<GrantedAuthority> authorities = new ArrayList<>();
+
+        for(Role role : user.getRoles()){
+            authorities.add(new SimpleGrantedAuthority("ROLE_" + role.getRole()));
+        }
+        return authorities;
+    }
+}
